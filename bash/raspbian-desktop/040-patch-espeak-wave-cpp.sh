06404134560c5ad68595f66ad286262a004624fd
Mike Ray <mike.ray@btinternet.com>
2014-03-09 12:52:40 +0000

    On branch master
     Changes to be committed:
    	deleted:    bash/raspbian-desktop/010-prep.sh
    	deleted:    bash/raspbian-desktop/020-libs.sh
    	deleted:    bash/raspbian-desktop/030-get-sources.sh
    	deleted:    bash/raspbian-desktop/040-patch-espeak-wave-cpp.sh
    	deleted:    bash/raspbian-desktop/050-patch-espeak-makefile.sh
    	deleted:    bash/raspbian-desktop/060-build-espeak.sh
    	deleted:    bash/raspbian-desktop/070-build-speech-dispatcher.sh
    	deleted:    bash/raspbian-desktop/075-install-speechd-up.sh
    	deleted:    bash/raspbian-desktop/080-dummy-espeak.sh
    	deleted:    bash/raspbian-desktop/090-dummy-speech-dispatcher.sh
    	deleted:    bash/raspbian-desktop/999-raspbian-desktop.sh

--- bash/raspbian-desktop/040-patch-espeak-wave-cpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WAVE_CPP=$(ls espeak*/src/wave.cpp)
-sed -i -e 's:#define FRAMES_PER_BUFFER 512:#define FRAMES_PER_BUFFER 2048:' \
-   -e 's:paFramesPerBufferUnspecified:FRAMES_PER_BUFFER:' \
-   -e 's:(double)0.1:(double)0.2:' \
-   -e 's:double aLatency = deviceInfo->defaultLowOutputLatency:double aLatency = deviceInfo->defaultHighOutputLatency:' "${WAVE_CPP}"
-
-exit 0
