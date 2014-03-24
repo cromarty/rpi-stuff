de335639b077065fbc7c7a84689a2e90ecc1882b
Viking <mike.ray@btinternet.com>
2014-03-24 22:34:52 +0000

    On branch master Changes to be committed:
    modified:   a11y-configs/pi-arch/console-espeakup/scripts/300-localisation.sh

--- a11y-configs/pi-arch/console-espeakup/scripts/300-localisation.sh
+++ a11y-configs/pi-arch/console-espeakup/scripts/300-localisation.sh
@@ -7,6 +7,8 @@ locale-gen
 localectl set-locale LANG="en_GB.UTF-8"
 timedatectl set-timezone Europe/London
 timedatectl set-ntp 1
+echo '-- Setting the keymap to UK...'
+localectl set-keymap --no-convert uk
 echo '-- Localisation changes will take effect on the next reboot'
 echo '-- Finished setting up localisation'
 exit 0
