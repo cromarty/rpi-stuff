a8f03a44190dedf90e723010d09e083af4915b75
Viking <mike.ray@btinternet.com>
2014-03-24 13:06:06 +0000

    On branch master Changes to be committed:
    new file:   a11y-configs/pi-arch/console-espeakup/config/etc/issue
    modified:   a11y-configs/pi-arch/console-espeakup/scripts/220-install-release-files.sh

--- a11y-configs/pi-arch/console-espeakup/scripts/220-install-release-files.sh
+++ a11y-configs/pi-arch/console-espeakup/scripts/220-install-release-files.sh
@@ -4,7 +4,7 @@ set -e
 cd "${BUILD_PATH}"
 echo "-- Installing the README file, if it exists, to '/home/pi'..."
 [ -f "${UTILS_PATH}/README" ] && install -m 644 "${UTILS_PATH}/README /home/pi
-echo "-- Installing the motd file if it exists..."
-[ -f "${UTILS_PATH}/motd" ] && install -m 644 "${UTILS_PATH}/motd" /etc/motd
+echo "-- Installing the 'issue' file if it exists..."
+[ -f "${CONFIG_PATH}/etc/issue" ] && install -m 644 "${CONFIG_PATH}/etc/issue" /etc/issue
 exit 0
 
