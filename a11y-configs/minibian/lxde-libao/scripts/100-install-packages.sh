836bb13b6a9fc2437741d9bad1757b51268b7888
Viking <mike.ray@btinternet.com>
2014-03-27 20:47:33 +0000

    modified:   a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/140-build-espeak.sh -> a11y-configs/minibian/lxde-libao/scripts/110-build-espeak.sh deleted:    a11y-configs/minibian/lxde-libao/scripts/110-uninstall-packages.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/150-build-speech-dispatcher.sh -> a11y-configs/minibian/lxde-libao/scripts/120-build-speech-dispatcher.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/160-build-speechd-up.sh -> a11y-configs/minibian/lxde-libao/scripts/130-build-speechd-up.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/170-configs.sh -> a11y-configs/minibian/lxde-libao/scripts/140-configs.sh

--- a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh
+++ a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh
@@ -2,8 +2,8 @@
 
 set -e
 cd "${BUILD_PATH}"
-echo '-- Installing some libraries and stuff necessary for the builds...'
-apt-get -y -q install equivs devscripts build-essential libasound2-plugins libportaudio2 portaudio19-dev libsonic0 libsonic-dev libdotconf-dev intltool libpulse-dev libao4 libao-dev libao-common libsndfile1 libsndfile1-dev
+echo '-- Installing some libraries and stuff necessary for the builds, a lengthy process...'
+apt-get -y -q install build-essential equivs devscripts libasound2-plugins libportaudio2 portaudio19-dev libsonic0 libsonic-dev libdotconf-dev intltool libpulse-dev libao4 libao-dev libao-common libsndfile1 libsndfile1-dev
 echo '-- Running autoremove to remove unnecessary packages...'
 apt-get -y -q autoremove
 echo '-- Finished installing packages'
