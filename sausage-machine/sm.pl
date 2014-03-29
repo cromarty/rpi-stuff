062de800ee7d13df34a85dc0f8e01f48de7428fc
Viking <mike.ray@btinternet.com>
2014-03-29 18:50:52 +0000

    Changes to sm.pl and sm.conf

--- sausage-machine/sm.pl
+++ sausage-machine/sm.pl
@@ -10,6 +10,7 @@ my $cfh = IO::File->new;
 my $key;
 my $val;
 my %cfg;
+my $true = 1;
 
 &load_config(\%cfg);
 
@@ -20,23 +21,21 @@ if ( ! &validate_config(\%cfg) ) {
 
 &set_env(\%cfg);
 
-
-#exit 0;
-
-&main_menu(\%cfg);
-
+while($true) {
+	$true = &main_menu(\%cfg);
+}
 
 sub main_menu {
 	my ($cfg) = @_;
+	my $answer = undef;
 	my $mainmenu = new Term::Menu(
-		tries => 3,
-		beforetext => 'Welcome to the Sausage machine, choose an option:',
-		aftertext => '? ',
-		nooptiontext => 'Invalid selection',
-		toomanytries => 'Retry count exceeded, Sausage machine will exit.'
+		tries => $cfg->{'tries'},
+		beforetext => "$cfg->{'main-beforetext'}",
+		aftertext => "$cfg->{'aftertext'}",
+		nooptiontext => "$cfg->{'nooptiontext'}",
+		toomanytries => "$cfg->{'toomanytries'}"
 		);
 
-	my $answer = undef;
 	$answer = $mainmenu->menu(
 		raspbian    => ["Raspbian configurations", '1'],
 		arch    => ["Arch configurations", '2'],
@@ -44,7 +43,12 @@ sub main_menu {
 		exit    => ["Exit", 'x'],
 	);
 	if ( defined $answer ) {
-		&{{ 'raspbian' => \&raspbian_menu($cfg, $answer), 'arch' => \&arch_menu($cfg, $answer), 'minibian' => \&minibian_menu($cfg, $answer), 'exit' => sub { print "Bye bye\n"; exit } }->{$answer}};
+		&{{
+			'raspbian' => sub { return &raspbian_menu($cfg, $answer) },
+			'arch' => sub { return &arch_menu($cfg, $answer) },
+			'minibian' => sub { return &minibian_menu($cfg, $answer) },
+			'exit' => sub { print "Bye bye\n"; return 0 }
+		}->{$answer}};
 	} else {
 		#print "Answer was undefined\n";
 	}
@@ -52,47 +56,92 @@ sub main_menu {
 
 sub raspbian_menu {
 	my ($cfg, $option) = @_;
-	my $menu = new Term::Menu( tries => 3);
 	my $answer = undef;
+	my $menu = new Term::Menu(
+		tries => "$cfg->{'tries'}",
+		beforetext => "$cfg->{'raspbian-beforetext'}",
+		aftertext => "$cfg->{'aftertext'}",
+		nooptiontext => "$cfg->{'nooptiontext'}",
+		toomanytries => "$cfg->{'toomanytries'}"
+	);
 	$answer = $menu->menu(
-		'lxde-sd-libao'    => ["Raspbian/LXDE/speech-dispatcher using libao", '1'],
+		'lxde-libao'    => ["Raspbian/LXDE/speech-dispatcher using libao", '1'],
 		'exit'    => ["Exit (back to main menu)", 'x'],
 	);
 	if ( defined $answer ) {
-		&{{ 'lxde-sd-libao' =>\&raspbian_lxde_sd_libao($cfg, "$option/$answer"), 'exit' => sub{} }->{$answer}};
+		if ( $answer eq 'exit' ) {
+			return 1;
+		} else {
+			return &sausages($cfg, "$option/$answer");
+		}
 	} else {
 		print "Answer was undefined\n";
+		return 1;
 	}
 } # raspbian_menu
 
 sub arch_menu {
 	my ($cfg, $option) = @_;
 	print "Arch was called\n";
+	return 0;
 	exit 0;
 } # arch_menu
 
 sub minibian_menu {
 	my ($cfg, $option) = @_;
 	print "Minibian was called\n";
+	return 0;
 	exit 0;
 } # minibian_menu
 
 #---- actual work
 
-sub raspbian_lxde_sd_libao {
+#sub raspbian_lxde_libao {
+sub sausages {
 	my ($cfg, $option) = @_;
 	my @scripts;
+	my $sm;
+	# note: by the time this function is called $option should be 'sausage-pack-root/category/pack'
+	# so appending /xxx will give the paths to config, utils, scripts and anything else
 	$ENV{CONFIG_PATH} = join("/", ($cfg->{'sausage-pack-root'},$option, 'config'));;
 	$ENV{SCRIPT_PATH} = join("/", ($cfg->{'sausage-pack-root'}, $option, 'scripts'));;
 	$ENV{UTILS_PATH} = join("/", ($cfg->{'sausage-pack-root'}, $option, 'utils'));;
-	@scripts = <$ENV{SCRIPT_PATH}/*.sh>;
-	foreach (@scripts) {
-		print "Script: $_\n";
-	}
-	print "Ran $option\n";
-	print "CONFIG_PATH: $ENV{CONFIG_PATH}\n";
-	print "SCRIPT_PATH: $ENV{SCRIPT_PATH}\n";
-	print "UTILS_PATH: $ENV{UTILS_PATH}\n";
+	$ENV{'SM_LOG_FILE'} = join("/", ($ENV{'BUILD_PATH'}, $cfg->{'log-file'}));
+	$sm = <<eof;
+
+sausages() {
+	echo 'Starting the sausage-machine...'
+	echo "The build-path is: $ENV{'BUILD_PATH'}"
+	echo "The config-path is: $ENV{'CONFIG_PATH'}"
+	echo "The script-path is: $ENV{'SCRIPT_PATH'}"
+	echo "The utils-path is: $ENV{'UTILS_PATH'}"
+
+	cd $ENV{'SCRIPT_PATH'}
+
+	shopt -s extglob
+
+	for SCRIPT_NAME in +([0-9][0-9][0-9])*.sh
+	do
+		echo "Starting \${SCRIPT_NAME}"
+		./\${SCRIPT_NAME}
+		if [ \$? -gt 0 ]; then
+			echo "\${SCRIPT_NAME} returned non-zero code, aborting..."
+			exit 1
+		fi
+		echo "\${SCRIPT_NAME} completed successfully"
+	done
+}
+
+sausages | tee -a $ENV{'SM_LOG_FILE'}
+
+eof
+
+
+
+
+
+
+	print "$sm\n";
 	exit 0;
 }
 
@@ -129,6 +178,11 @@ sub validate_config {
 		return 0;
 	}
 
+	if ( ! $cfg->{'log-file'} ) {
+		print "ERROR: Did not find a log-file key in the config file, setting to the default 'sausage-machine.log'\n";
+		$cfg->{'log-file'} = "sausage-machine.log";
+	}
+
 	return 1;
 }
 
@@ -136,9 +190,8 @@ sub set_env {
 	my ($cfg) = 	@_;
 	$ENV{'BUILD_PATH'} = $cfg->{'build-path'};
 	$ENV{'SAUSAGE_PACK_ROOT'} = $cfg->{'sausage-pack-root'};
+	$ENV{'SM_LOG_FILE'} = $cfg->{'log-file'};
 	return;
 }
-
-
 
 
