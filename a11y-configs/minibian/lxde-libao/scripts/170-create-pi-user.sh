e8e94402ef48cf7c192085ddbaa3c4de80d76389
Viking <mike.ray@btinternet.com>
2014-04-01 12:26:08 +0100

    wheel stuff

--- a11y-configs/minibian/lxde-libao/scripts/170-create-pi-user.sh
+++ a11y-configs/minibian/lxde-libao/scripts/170-create-pi-user.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo "-- Creating regular user 'pi'..."
-useradd -m -g users -G adm,dialout,cdrom,sudo,audio,video,plugdev,games -s /bin/bash pi
+useradd -m -g users -G adm,dialout,cdrom,wheel,audio,video,plugdev,games -s /bin/bash pi
 echo -e "raspberry\nraspberry\n" | passwd pi 
 echo "-- Created user 'pi' successfully"
 exit 0
