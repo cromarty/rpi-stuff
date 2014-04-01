3b2b5153947fbd3ae672aebb87251e6459212aca
Viking <mike.ray@btinternet.com>
2014-04-01 14:18:26 +0100

    modified:   a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh

--- a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh
+++ a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh
@@ -2,14 +2,18 @@
 
 set -e
 cd "${BUILD_PATH}"
-echo "-- Adding 'export DTK_PROGRAM=espeak' to /etc/skel.bashrc..."
-cat <<eof >> /etc/skel/.bashrc
+echo "-- Adding speakupconf and DTK_PROGRAM stuff to .bashrc..."
+cat <<eof > /etc/skel/.bashrc
+
+if [ -d ~/.speakup ]; then
+	speakupconf load
+fi
 
 export DTK_PROGRAM=espeak
 
 eof
 
-echo "-- Finished adding 'DTK_PROGRAM=espeak' to /etc/skel/.bashrc"
+echo "-- Finished adding speakup and Emacspeak stuff to /etc/skel/.bashrc"
 exit 0
 
 
