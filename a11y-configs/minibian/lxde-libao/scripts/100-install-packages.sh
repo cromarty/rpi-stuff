a4d4f366d09f87949d2a6f7f5725e0cc9f3124cf
Viking <mike.ray@btinternet.com>
2014-04-01 09:57:57 +0100

    modified:   a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/310-speakup-modules.sh -> a11y-configs/minibian/lxde-libao/scripts/310-modules.sh

--- a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh
+++ a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh
@@ -3,7 +3,7 @@
 set -e
 cd "${BUILD_PATH}"
 echo '-- Installing some libraries and stuff necessary for the builds, a lengthy process...'
-apt-get -y -q install build-essential equivs devscripts libasound2-plugins libportaudio2 portaudio19-dev libsonic0 libsonic-dev libdotconf-dev intltool libpulse-dev libao4 libao-dev libao-common libsndfile1 libsndfile1-dev
+apt-get -y -q install build-essential equivs devscripts alsa-utils libasound2-plugins libportaudio2 portaudio19-dev libsonic0 libsonic-dev libdotconf-dev intltool libpulse-dev libao4 libao-dev libao-common libsndfile1 libsndfile1-dev
 echo '-- Running autoremove to remove unnecessary packages...'
 apt-get -y -q autoremove
 echo '-- Finished installing packages'
--- a11y-configs/minibian/lxde-libao/scripts/310-modules.sh
+++ a11y-configs/minibian/lxde-libao/scripts/310-modules.sh
@@ -2,5 +2,6 @@
 set -e
 cd "${BUILD_PATH}"
 echo "-- Inserting speakup_soft into /etc/modules..."
+echo "snd-bcm2835" >> /etc/modules
 echo "speakup_soft" >> /etc/modules
 exit 0
