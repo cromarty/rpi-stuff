25e015186ad7c635e438cbae6288ced19b17d18a
Viking <mike.ray@btinternet.com>
2014-04-01 12:22:38 +0100

    modified:   a11y-configs/minibian/lxde-libao/scripts/100-install-packages.sh new file:   a11y-configs/minibian/lxde-libao/scripts/160-grant-sudo-privilages.sh renamed:    a11y-configs/minibian/lxde-libao/scripts/160-create-pi-user.sh -> a11y-configs/minibian/lxde-libao/scripts/170-create-pi-user.sh modified:   a11y-configs/minibian/lxde-libao/scripts/310-modules.sh

--- a11y-configs/minibian/lxde-libao/scripts/310-modules.sh
+++ a11y-configs/minibian/lxde-libao/scripts/310-modules.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 cd "${BUILD_PATH}"
-echo "-- Inserting speakup_soft into /etc/modules..."
+echo "-- Inserting snd-bcm2835 and speakup_soft into /etc/modules..."
 echo "snd-bcm2835" >> /etc/modules
 echo "speakup_soft" >> /etc/modules
+echo '-- Try to load the sound driver...'
+set +e ; modprobe snd-bcm2835 ; set -e
+echo '-- Completed module insertions'
 exit 0
