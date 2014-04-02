1785f355aaf9ca28cedd1170e5ba305640a9a919
Viking <mike.ray@btinternet.com>
2014-04-02 15:15:40 +0100

    modified:   a11y-configs/pi-arch/console-sd-libao/scripts/180-configure-emacspeak.sh

--- a11y-configs/pi-arch/console-sd-libao/scripts/180-configure-emacspeak.sh
+++ a11y-configs/pi-arch/console-sd-libao/scripts/180-configure-emacspeak.sh
@@ -5,7 +5,7 @@ cd "${BUILD_PATH}"
 echo '-- Installing some emacspeak stuff into /etc/skel...'
 install -m644 ${CONFIG_PATH}/emacspeak/.emacs /etc/skel
 mkdir -p /etc/skel/.emacs.d/elpa
-install -m644 "${CONFIG_PATH}/emacspeak/package.el /etc/skel/.emacs.d/elpa
+install -m644 "${CONFIG_PATH}/emacspeak/package.el" /etc/skel/.emacs.d/elpa
 echo '-- Finished installing emacspeak files to /etc/skel'
 exit 0
 
