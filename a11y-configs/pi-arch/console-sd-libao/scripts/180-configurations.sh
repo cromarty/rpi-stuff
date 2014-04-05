e47b2bc691f998cc66af5439af545da231aad288
Viking <mike.ray@btinternet.com>
2014-04-05 23:48:53 +0100

    modified:   a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh

--- a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh
+++ a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh
@@ -6,7 +6,17 @@ echo '-- Installing some emacspeak stuff into /etc/skel...'
 install -m644 ${CONFIG_PATH}/emacspeak/.emacs /etc/skel
 mkdir -p /etc/skel/.emacs.d/elpa
 install -m644 "${CONFIG_PATH}/emacspeak/package.el" /etc/skel/.emacs.d/elpa
-echo '-- Finished installing emacspeak files to /etc/skel'
+
+echo '-- Installing speech-dispatcher configuration file in /etc/speech-dispatcher...'
+install -m644 "${CONFIG_PATH}/speech-dispatcher/speechd.conf" /etc/speech-dispatcher
+echo '-- Installing speechd-up.conf into /etc/speechd-up.conf...'
+install -m644 "${CONFIG_PATH}/speechd-up/speechd-up.conf" /etc
+
+echo '-- Duplicating default speech-dispatcher configuration for user pi...'
+mkdir -p /home/pi/.speech-dispatcher/conf
+cp -r /etc/speech-dispatcher/* /home/pi/.speech-dispatcher/conf
+chown -R pi:users /home/pi/.speech-dispatcher
+
 exit 0
 
 
