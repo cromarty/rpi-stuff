e3f89d042347cd3bb4a51415f79a2acfb6381c53
Viking <mike.ray@btinternet.com>
2014-04-06 22:53:26 +0100

    modified:   a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh new file:   a11y-configs/pi-arch/console-sd-libao/utils/speakupconf

--- a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh
+++ a11y-configs/pi-arch/console-sd-libao/scripts/180-configurations.sh
@@ -8,20 +8,24 @@ mkdir -p /etc/skel/.emacs.d/elpa
 install -m644 "${CONFIG_PATH}/emacspeak/package.el" /etc/skel/.emacs.d/elpa
 
 echo '-- Installing speech-dispatcher configuration file in /etc/speech-dispatcher...'
-install -m644 "${CONFIG_PATH}/speech-dispatcher/speechd.conf" /etc/speech-dispatcher
+install -m 644 "${CONFIG_PATH}/speech-dispatcher/speechd.conf" /etc/speech-dispatcher
+
 echo '-- Installing speechd-up.conf into /etc/speechd-up.conf...'
-install -m644 "${CONFIG_PATH}/speechd-up/speechd-up.conf" /etc
+install -m 644 "${CONFIG_PATH}/speechd-up/speechd-up.conf" /etc
+
+echo '-- Installing speakupconf...'
+install -m 755 "${UTILS_PATH}/speakupconf" /usr/bin
 
 
-echo "-- Creating the speech-dispatcher user and it's home directory..."
+echo '-- Creating some soft links to fool sd...'
+mkdir /usr/lib/arm-linux-gnueabihf
+ln -s /usr/lib/speech-dispatcher-modules /usr/lib/arm-linux-gnueabihf
+ln -s /usr/lib/ao /usr/lib/arm-linux-gnueabihf
 
-echo '-- Creating the speech-dispatcher user...'
-useradd --system \
-	--comment 'speech-dispatcher' \ 
-	--gid audio \
-	--no-log-init \
-	--no-user-group \
-	--gecos 'speech-dispatcher' speech-dispatcher
+echo '-- Removing some annoying and unnecessary sd modules...'
+rm /usr/lib/speech-dispatcher-modules/sd_cicero
+rm /usr/lib/speech-dispatcher-modules/sd_festival
+rm /usr/lib/speech-dispatcher-modules/sd_generic
 
 exit 0
 
