7da6f0359fc41355645cb829257fe97bf6391943
Viking <mike.ray@btinternet.com>
2014-04-07 19:02:34 +0100

    modified:   a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh modified:   a11y-configs/pi-arch/console-sd-libao/utils/README

--- a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh
+++ a11y-configs/pi-arch/console-sd-libao/scripts/200-edit-skel-files.sh
@@ -3,13 +3,23 @@
 set -e
 cd "${BUILD_PATH}"
 echo "-- Adding speakupconf and DTK_PROGRAM stuff to .bashrc..."
-cat <<eof > /etc/skel/.bashrc
+cat <<eof >> /etc/skel/.bashrc
 
 if [ -d ~/.speakup ]; then
 	speakupconf load
 fi
 
 export DTK_PROGRAM=espeak
+
+# clear bash history on login
+> ~/.bash_history && history -c
+
+eof
+echo '-- Adding some stuff to .bash_logout...'
+cat <<eof >> /etc/skel/.bash_logout
+
+# clear the bash history on logout
+> ~/.bash_history && history -c
 
 eof
 
