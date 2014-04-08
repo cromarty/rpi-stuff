12e364a6cc7935d1175c99693883f86484b79033
Viking <mike.ray@btinternet.com>
2014-04-08 20:18:05 +0100

    Changed alarmpi/console-sd-libao/scripts/050-get-local-packages.sh

--- a11y-configs/alarmpi/console-sd-libao/scripts/050-get-local-packages.sh
+++ a11y-configs/alarmpi/console-sd-libao/scripts/050-get-local-packages.sh
@@ -4,10 +4,9 @@ set -e
 echo "-- Getting packages from: ${PACKAGE_URL}..."
 cd "${PACKAGE_PATH}"
 
-wget http://${PACKAGE_URL}/get-packages.sh
-chmod +x get-packages.sh
+wget http://${PACKAGE_URL}/get-packages.sub
 
-./get-packages.sh
+[ -f "./get-packages.sub" ] && . "./get-packages.sub"
 
 echo "-- Finished getting packages from ${PACKAGE_URL}"
 exit 0
