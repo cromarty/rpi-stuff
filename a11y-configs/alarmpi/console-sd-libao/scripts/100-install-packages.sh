2b6345e67fca383e3077894393165f64f028dcb3
Viking <mike.ray@btinternet.com>
2014-04-08 23:57:10 +0100

    modified:   a11y-configs/alarmpi/console-sd-libao/scripts/100-install-packages.sh

--- a11y-configs/alarmpi/console-sd-libao/scripts/100-install-packages.sh
+++ a11y-configs/alarmpi/console-sd-libao/scripts/100-install-packages.sh
@@ -3,7 +3,7 @@
 set -e
 cd ${BUILD_PATH}
 echo '-- Installing a bunch of needed packages, a lengthy process...'
-pacman -S --noconfirm --noprogressbar --needed base-devel unzip parted git portaudio alsa-utils bc gnutls fuse sshfs tcl tk emacs-nox yajl icu libao dotconf intltool
+pacman -S --noconfirm --noprogressbar --needed base-devel unzip parted git portaudio alsa-utils bc gnutls fuse sshfs tcl tk emacs-nox yajl icu libao dotconf intltool python2
 echo '-- Finished installing packages'
 exit 0
 
