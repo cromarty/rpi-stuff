2551b73e569deb17e7e98aef84f33e87c494f4b6
Viking <mike.ray@btinternet.com>
2014-04-11 22:27:57 +0100

    modified:   a11y-configs/alarmpi/console-sd-libao/scripts/230-install-release-files.sh

--- a11y-configs/alarmpi/console-sd-libao/scripts/230-install-release-files.sh
+++ a11y-configs/alarmpi/console-sd-libao/scripts/230-install-release-files.sh
@@ -3,8 +3,12 @@
 set -e
 cd "${BUILD_PATH}"
 echo '-- Installing release files...' 
-#[ -f "${RELEASE_PATH}/README" ] && install -m 644 -o pi -g users "${RELEASE_PATH}/README" /home/pi
-install -m 644 -o pi -g users "${RELEASE_PATH}/*" /home/pi
+pushd "${RELEASE_PATH}" >/dev/null
+for RF in *
+do
+	install -m 644 -o pi -g users "${RF}" /home/pi
+done
+popd >/dev/null
 
 echo "-- Installing the 'issue' file if it exists..."
 [ -f "${RELEASE_PATH}/issue" ] && install -m 644 "${RELEASE_PATH}/issue" /etc/issue
