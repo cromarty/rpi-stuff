6fae31e3f7a40a92204d0a9459c263802bb2cd52
Viking <mike.ray@btinternet.com>
2014-04-17 02:54:38 +0100

    new file:   a11y-configs/raspbian/lxde-libao/config/.xsession modified:   a11y-configs/raspbian/lxde-libao/config/espeak.conf modified:   a11y-configs/raspbian/lxde-libao/scripts/800-install-gnome-orca.sh

--- a11y-configs/raspbian/lxde-libao/scripts/800-install-gnome-orca.sh
+++ a11y-configs/raspbian/lxde-libao/scripts/800-install-gnome-orca.sh
@@ -4,4 +4,13 @@ set -e
 cd "${BUILD_PATH}"
 echo "-- Installing gnome-orca..."
 apt-get -y -q install gnome-orca
+
+echo '-- Creating some soft links to fool gtk module loading...'
+ln -s arm-linux-gnueabihf/gtk-2.0 /usr/lib
+ln -s arm-linux-gnueabihf/gtk-3.0 /usr/lib
+
+echo '-- Installing /home/pi/.xsession...'
+install -m 644 -o pi -g users ${CONFIG_PATH}/.xsession /home/pi
+
+echo '-- Finished installing Orca'
 exit 0
