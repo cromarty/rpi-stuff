e6e4dd66be8e15e0311de849f8eca94a4d4272a2
Mike Ray <mike.ray@btinternet.com>
2014-04-18 09:32:23 +0100

    modified:   a11y-configs/alarmpi/lxde/scripts/100-install-packages.sh

--- a11y-configs/alarmpi/lxde/scripts/100-install-packages.sh
+++ a11y-configs/alarmpi/lxde/scripts/100-install-packages.sh
@@ -3,9 +3,9 @@
 set -e
 echo '-- Installing LXDE and related stuff...'
 sudo pacman -S --noconfirm --noprogressbar xorg-server xorg-xinit xorg-utils xorg-server-utils 
-sudo pacman -S --noconfirm --noprogressbar xorg-xinit xorg-xclock xorg-twm xterm
 sudo pacman -S --noconfirm --noprogressbar xf86-video-fbdev 
-sudo pacman -S --noconfirm --noprogressbar lxde
+pacman -S --noconfirm --noprogressbar libfm lxappearance lxappearance-obconf lxde-common lxde-icon-theme lightdm lxinput lxlauncher lxmenu-data lxmusic lxpanel lxpolkit lxsession lxtask lxterminal menu-cache openbox pcmanfm
+pacman -S --noconfirm --noprogressbar leafpad
 
 echo '-- Setting up /home/pi/.xinitrc...'
 cat <<eof > /home/pi/.xinitrc
