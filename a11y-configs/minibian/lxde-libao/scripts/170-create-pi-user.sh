f35bc46c4ce183e21d7bc1e53bb197e9ad68bfa5
Mike Ray <mike.ray@btinternet.com>
2016-08-04 19:32:22 +0100

    Removed the defunct Minibian sausage-pack

--- a11y-configs/minibian/lxde-libao/scripts/170-create-pi-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-echo "-- Creating regular user 'pi'..."
-useradd -m -g users -G adm,dialout,cdrom,wheel,audio,video,plugdev,games -s /bin/bash pi
-echo -e "raspberry\nraspberry\n" | passwd pi 
-echo "-- Created user 'pi' successfully"
-exit 0
- 
