d8fa7e97bbd9606c50afeceb5c95aa5fe6e6fe07
Mike Ray <mike.ray@btinternet.com>
2017-04-15 13:49:59 +0100

    On branch new_imgsync Changes to be committed:
    	new file:   backuppi
    	modified:   imgsync
    	modified:   imgtool
    	modified:   imgtool.lib
    	new file:   lib/check_root
    	new file:   lib/clean_up
    	new file:   lib/loop_devices
    	new file:   lib/make_lib.sh
    	new file:   lib/mount_device
    	new file:   lib/mount_image
    	new file:   lib/mount_partitions
    	new file:   lib/new_image
    	new file:   lib/remove_loop_devices
    	new file:   lib/remove_mount_points
    	new file:   lib/un_mount_partitions
    	new file:   lib/validate_blockcount
    	new file:   lib/validate_blocksize
    	new file:   lib/validate_mountpoint
    	new file:   lib/validate_split
    	new file:   lib/volume_is_mounted

---
+++ imgtool/lib/make_lib.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat check_root > ../imgtool.lib
+cat clean_up >> ../imgtool.lib
+cat loop_devices >> ../imgtool.lib
+cat mount_device >> ../imgtool.lib
+cat mount_image >> ../imgtool.lib
+cat mount_partitions >> ../imgtool.lib
+cat new_image >> ../imgtool.lib
+cat remove_loop_devices >> ../imgtool.lib
+cat remove_mount_points >> ../imgtool.lib
+cat un_mount_partitions >> ../imgtool.lib
+cat validate_blockcount >> ../imgtool.lib
+cat validate_blocksize >> ../imgtool.lib
+cat validate_mountpoint >> ../imgtool.lib
+cat validate_split >> ../imgtool.lib
+cat volume_is_mounted >> ../imgtool.lib
